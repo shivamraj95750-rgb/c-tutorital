62fc5873452b155a2097bccc36c238237cfaae1c
Shivam <shivamraj95750@gmail.com>
2026-06-07 21:54:02 +0530

    second program

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a , b;
+	printf("enter a two number");
+	scanf("%d%d",&a,&b);
+	printf("%daddition\n",a+b);
+	printf("%dsubstraction\n",a-b);
+	printf("%dmultiplication\n",a*b);
+	printf("%ddivision\n",a/b);
+	return 0;
+}
